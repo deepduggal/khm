ebaee3f05853214778dafc9ef491ac78e124b601
waxter013 <duggald14@gmail.com>
2018-06-06 15:30:49 -0400

    Added SSR via react-snapshot.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import 'material-components-web/dist/material-components-web.min.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { render } from 'react-snapshot';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
 registerServiceWorker();
